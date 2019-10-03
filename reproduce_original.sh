3458cf0d2b2041e2bf5f7414850d7a6fa2dc0a23
Nicolas Garneau <nicolas.garneau.1@ulaval.ca>
2019-10-03 15:27:33 -0400

    Time commands for comparison with original results

--- reproduce_original.sh
+++ reproduce_original.sh
@@ -13,7 +13,7 @@ do
     while [ $counter -le $num_runs ]
     do
         echo "Running experiment for $source_language, $target_language, $counter"
-        python3 map_embeddings.py --acl2018 ./data/embeddings/${source_language}.emb.txt ./data/embeddings/${target_language}.emb.txt ./output/${source_language}.${target_language}.${counter}.emb.txt ./output/${target_language}.${source_language}.${counter}.emb.txt
+        time python3 map_embeddings.py --acl2018 ./data/embeddings/${source_language}.emb.txt ./data/embeddings/${target_language}.emb.txt ./output/${source_language}.${target_language}.${counter}.emb.txt ./output/${target_language}.${source_language}.${counter}.emb.txt
         python3 eval_translation.py ./output/${source_language}.${target_language}.${counter}.emb.txt ./output/${target_language}.${source_language}.${counter}.emb.txt ./data/dictionaries/${source_language}-${target_language}.test.txt
         ((counter++))
     done
