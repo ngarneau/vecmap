8fcaf048a9883be819cb4b5267b10d40b9f8738e
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2019-10-18 09:38:59 -0400

    Update beluga_launcher

--- beluga_launcher.sh
+++ beluga_launcher.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #SBATCH --account=def-lulam50
+#SBATCH --cpus-per-task=6
 #SBATCH --gres=gpu:1              # Number of GPUs (per node)
-#SBATCH --mem=6000M               # memory (per node)
+#SBATCH --mem=6G                  # memory (per node)
 #SBATCH --time=0-05:00            # time (DD-HH:MM)
 
 python -m src.scripts.experiment -u with seed_dictionary_method='unsupervised' cuda=True num_runs=1
