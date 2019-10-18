fe0d86f74015ec41f169f705f879d4883497ecb5
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2019-10-18 10:04:35 -0400

    Group .out in one folder

--- beluga_launcher.sh
+++ beluga_launcher.sh
@@ -4,6 +4,7 @@
 #SBATCH --gres=gpu:1              # Number of GPUs (per node)
 #SBATCH --mem=6G                  # memory (per node)
 #SBATCH --time=0-05:00            # time (DD-HH:MM)
+#SBATCH --output=$HOME/vecmap/run_outputs/%j.out
 
 source $HOME/venv/bin/activate
 python -m src.scripts.experiment -u with seed_dictionary_method='unsupervised' cuda=True num_runs=1
