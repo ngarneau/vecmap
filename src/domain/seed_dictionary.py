ffad8423224b234fd271176131e538658769b211
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2019-10-19 15:52:36 -0400

    Add random_raw and random_cutoff seed dicts

--- src/domain/seed_dictionary.py
+++ src/domain/seed_dictionary.py
@@ -1,5 +1,6 @@
 import logging
 from typing import Tuple, List
+import numpy as np
 import re
 
 import embeddings
@@ -16,6 +17,7 @@ class SeedDictionary:
     def get_indices(self) -> Tuple[List[int], List[int]]:
         pass
 
+
 class UnsupervisedSeedDictionary(SeedDictionary):
     def __init__(self, xp, src_words, trg_words, x, z, configurations):
         super().__init__(src_words, trg_words)
@@ -25,11 +27,12 @@ class UnsupervisedSeedDictionary(SeedDictionary):
         self.z = z
 
     def get_indices(self):
-        sim_size = min(self.x.shape[0], self.z.shape[0]) if self.configurations['unsupervised_vocab'] <= 0 else min(self.x.shape[0], self.z.shape[0], self.configurations['unsupervised_vocab'])
+        sim_size = min(self.x.shape[0], self.z.shape[0]) if self.configurations['unsupervised_vocab'] <= 0 else min(
+            self.x.shape[0], self.z.shape[0], self.configurations['unsupervised_vocab'])
         u, s, vt = self.xp.linalg.svd(self.x[:sim_size], full_matrices=False)
-        xsim = (u*s).dot(u.T)  # This is equivalent to Mx in the original paper
+        xsim = (u * s).dot(u.T)  # This is equivalent to Mx in the original paper
         u, s, vt = self.xp.linalg.svd(self.z[:sim_size], full_matrices=False)
-        zsim = (u*s).dot(u.T)  # This is equivalent to Mz in the original paper
+        zsim = (u * s).dot(u.T)  # This is equivalent to Mz in the original paper
         del u, s, vt
         xsim.sort(axis=1)
         zsim.sort(axis=1)
@@ -40,7 +43,7 @@ class UnsupervisedSeedDictionary(SeedDictionary):
         if self.configurations['csls'] > 0:
             knn_sim_fwd = topk_mean(sim, k=self.configurations['csls'])
             knn_sim_bwd = topk_mean(sim.T, k=self.configurations['csls'])
-            sim -= knn_sim_fwd[:,self.xp.newaxis]/2 + knn_sim_bwd/2
+            sim -= knn_sim_fwd[:, self.xp.newaxis] / 2 + knn_sim_bwd / 2
 
         if self.configurations['direction'] == 'forward':
             src_indices = self.xp.arange(sim_size)
@@ -91,7 +94,8 @@ class DefaultSeedDictionary(SeedDictionary):
     def __init__(self, xp, src_words, trg_words, x, z, configurations):
         super().__init__(src_words, trg_words)
         self.configurations = configurations
-        self.dictionary_filename = './data/dictionaries/{}-{}.train.txt'.format(configurations['source_language'], configurations['target_language'])  # the training dictionary file
+        self.dictionary_filename = './data/dictionaries/{}-{}.train.txt'.format(
+            configurations['source_language'], configurations['target_language'])  # the training dictionary file
 
     def get_indices(self):
         f = open(self.dictionary_filename, encoding=self.configurations['encoding'], errors='surrogateescape')
@@ -106,4 +110,57 @@ class DefaultSeedDictionary(SeedDictionary):
                 trg_indices.append(trg_ind)
             except KeyError:
                 logging.warning('WARNING: OOV dictionary entry ({0} - {1})'.format(src, trg))
+        return src_indices, trg_indices
+
+
+class RandomRawSeedDictionary(SeedDictionary):
+    def __init__(self, xp, src_words, trg_words, x, z, configurations):
+        super().__init__(src_words, trg_words)
+        self.configurations = configurations
+
+    def get_indices(self):
+        src_indices = list()
+        trg_indices = list()
+
+        if len(self.src_words) > len(self.trg_words):
+            random_mapping = np.random.randint(len(self.trg_words), size=len(self.src_words))
+
+            for src_index, trg_index in enumerate(random_mapping):
+                src_indices.append(src_index)
+                trg_indices.append(trg_index)
+        else:
+            random_mapping = np.random.randint(len(self.src_words), size=len(self.trg_words))
+
+            for trg_index, src_index in enumerate(random_mapping):
+                src_indices.append(src_index)
+                trg_indices.append(trg_index)
+
+        return src_indices, trg_indices
+
+
+class RandomCutoffSeedDictionary(SeedDictionary):
+    def __init__(self, xp, src_words, trg_words, x, z, configurations):
+        super().__init__(src_words, trg_words)
+        self.configurations = configurations
+
+    def get_indices(self):
+        self.src_words = self.src_words[:self.configurations['vocabulary_cutoff']]
+        self.trg_words = self.trg_words[:self.configurations['vocabulary_cutoff']]
+
+        src_indices = list()
+        trg_indices = list()
+
+        if len(self.src_words) > len(self.trg_words):
+            random_mapping = np.random.randint(len(self.trg_words), size=len(self.src_words))
+
+            for src_index, trg_index in enumerate(random_mapping):
+                src_indices.append(src_index)
+                trg_indices.append(trg_index)
+        else:
+            random_mapping = np.random.randint(len(self.src_words), size=len(self.trg_words))
+
+            for trg_index, src_index in enumerate(random_mapping):
+                src_indices.append(src_index)
+                trg_indices.append(trg_index)
+
         return src_indices, trg_indices
--- src/factory/seed_dictionary.py
+++ src/factory/seed_dictionary.py
@@ -6,6 +6,8 @@ class SeedDictionaryBuilderFactory:
     UNSUPERVISED = 'unsupervised'
     NUMERALS = 'numerals'
     IDENTICAL = 'identical'
+    RANDOM_RAW = 'random_raw'
+    RANDOM_CUTOFF = 'random_cutoff'
     DEFAULT = 'default'
 
     @classmethod
@@ -16,7 +18,11 @@ class SeedDictionaryBuilderFactory:
             return NumeralsSeedDictionary(xp, src_words, trg_words, x, z, configurations)
         elif method == cls.IDENTICAL:
             return IdenticalSeedDictionary(xp, src_words, trg_words, x, z, configurations)
+        elif method == cls.RANDOM_RAW:
+            return RandomRawSeedDictionary(xp, src_words, trg_words, x, z, configurations)
+        elif method == cls.RANDOM_CUTOFF:
+            return RandomCutoffSeedDictionary(xp, src_words, trg_words, x, z, configurations)
         elif method == cls.DEFAULT:
             return DefaultSeedDictionary(xp, src_words, trg_words, x, z, configurations)
         else:
-            raise("Method {} not implemented.".format(method))
+            raise ("Method {} not implemented.".format(method))
