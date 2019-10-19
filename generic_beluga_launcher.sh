24bc730d9cd6a38f97ba5ad9a122e751fabe78fa
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2019-10-19 16:47:05 -0400

    add email failure notifications to beluga laucnher

--- generic_beluga_launcher.sh
+++ generic_beluga_launcher.sh
@@ -4,6 +4,8 @@
 #SBATCH --gres=gpu:1              # Number of GPUs (per node)
 #SBATCH --mem=5G                  # memory (per node)
 #SBATCH --time=0-05:00            # time (DD-HH:MM)
+#SBATCH --mail-user=mathieu.godbout.3@ulaval.ca
+#SBATCH --mail-type=FAIL
 
 source $HOME/venv/bin/activate
 python -m src.scripts.experiment $@
