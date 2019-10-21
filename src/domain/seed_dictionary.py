81e5b10a30e82354151478aae0434af0f069fdb5
davebulaval <david.beauchemin.5@ulaval.ca>
2019-10-21 11:14:15 -0400

    form definition of an abstract class for SeedDictionnary

--- src/domain/seed_dictionary.py
+++ src/domain/seed_dictionary.py
@@ -1,18 +1,21 @@
 import logging
 from typing import Tuple, List
 import re
+from abc import ABC, abstractmethod
+from typing import Tuple, List
 
 import embeddings
 from src.utils import topk_mean
 
 
-class SeedDictionary:
+class SeedDictionary(ABC):
     def __init__(self, src_words, trg_words):
         self.src_words = src_words
         self.trg_words = trg_words
         self.src_word2ind = {word: ind for ind, word in enumerate(self.src_words)}
         self.trg_word2ind = {word: ind for ind, word in enumerate(self.trg_words)}
 
+    @abstractmethod
     def get_indices(self) -> Tuple[List[int], List[int]]:
         pass
 
