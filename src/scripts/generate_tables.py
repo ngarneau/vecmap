2cabc8f96386be2a1eaa0d9401282f8d0ff041cd
Nicolas Garneau <nicolas.garneau.1@ulaval.ca>
2019-11-04 11:18:14 -0500

    Adding the number of successful runs as a ratio

--- src/scripts/generate_tables.py
+++ src/scripts/generate_tables.py
@@ -29,8 +29,8 @@ def generate_table_3(configs):
 
 def main():
     base_configs = yaml.load(open('./configs/base.yaml'), Loader=yaml.FullLoader)
-    # generate_table_1(base_configs)
-    # generate_table_2(base_configs)
+    generate_table_1(base_configs)
+    generate_table_2(base_configs)
     generate_table_3(base_configs)
 
 if __name__ == '__main__':
