bceca7824b7688aa8ed536194bc8459f4438d271
Nicolas Garneau <nicolas.garneau.1@ulaval.ca>
2019-11-19 10:50:38 -0500

    Packaging of the project for the challenge

--- image/run.sh
+++ image/run.sh
@@ -1,3 +1,3 @@
-pip install -r requirements.txt
-python -m src.scripts.runner --input_path=/input --output_path=/output
-python -m src.scripts.runner --input_path=/input --output_path=/output
+pip install -r nvidia_requirements.txt
+python -m src.scripts.runner --input_path=/input --output_path=/output --cuda=True
+python -m src.scripts.generate_tables --input_path=/input --output_path=/output
